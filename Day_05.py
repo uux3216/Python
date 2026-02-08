e0e3a76bb30fb76579aa5785c81408f0baec8f0f
uux3216 <uux3216@gmail.com>
2026-02-08 21:28:55 +0600

    Add files via upload

---
+++ Day_05.py
@@ -0,0 +1,28 @@
+# Zero 2 Hero (Day 05)
+print("Zero 2 Hero (Day 05)")
+
+# List
+friends = ["Rahim", "Karim", "Sujon", "Balam"]
+print(friends)
+
+# List Indexing
+friends = ["Rahim", "Karim", "Sujon", "Balam"]
+print(friends[1])
+
+# Change List Item
+friends = ["Rahim", "Karim", "Sujon", "Balam"]
+
+friends[1] = "Noyon"
+print(friends)
+
+# Add List Item
+friends = ["Rahim", "Karim", "Sujon", "Balam"]
+
+friends.append("Raju")
+print(friends)
+
+# Add List Item
+friends = ["Rahim", "Karim", "Sujon", "Balam"]
+
+friends.insert(0, "Tanvir")
+print(friends)
