6112a6f6c05c254162eb4c437e6581b74aa137ff
uux3216 <uux3216@gmail.com>
2026-02-08 21:22:35 +0600

    Add files via upload

---
+++ Day_01.py
@@ -0,0 +1,29 @@
+# Zero 2 Hero (Day 01)
+print("Zero 2 Hero (Day 01)")
+
+# Data Type String
+Name = "Abdullah Al Mamun"
+print("My name is", Name)
+
+# Data Type Integer
+Age = 25
+print("My age is", Age)
+
+# Data Type Float
+Height = 5.7
+print("My height is", Height, "inch")
+
+# Data Type Boolean
+Is_Student = True
+print("I am a student", Is_Student)
+
+# Data Type Boolean
+Is_Terrorist = False
+print("I am a terrorist", Is_Terrorist)
+
+#Day 01, Task 01
+brand = "Vivo Y20"
+price = 20000
+camera = 25.5
+
+print(f"My favorite phone is {brand}, price is {price} and camera is {camera} MP.")
