dcf286b9e0f338d89f0097f911307df36c277f9e
uux3216 <uux3216@gmail.com>
2026-02-08 21:27:37 +0600

    Add files via upload

---
+++ Day_02.py
@@ -0,0 +1,20 @@
+# Zero 2 Hero (Day 02)
+print("Zero 2 Hero (Day 02)")
+
+# User Input String
+name = input(str("Enter your name:" ))
+print("Welcome", name)
+
+# User Input Integer
+age = int(input("Enter your age:" ))
+print("Your age", age)
+
+# User Input Float
+height = float(input("Enter your height:" ))
+print("Your height", height)
+
+#Day 01, Task 02
+money = float(input("How much $ do you have?" ))
+result = money * 120
+
+print(f"You have {result} Taka")
